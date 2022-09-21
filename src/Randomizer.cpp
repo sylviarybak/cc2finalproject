1db486bd59e7833aa1d9fdabe3a9693b576ee78a
Your Name <rybaksylviaphoto@gmail.com>
2022-09-22 00:07:19 +0200

    here we gooooooo

--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -21,9 +21,9 @@ void Randomizer::update() {
 
 void Randomizer::draw() {
 
-	// this runs in the draw but then it runs every frame which is not cool, if i move it out to setup or smth, the camera input doesnt work
+	// this runs in the draw but then it runs every frame which is not cool, if i move it out to setup or smth, the camera input doesnt work, nor will any other draws
 
-	// float rounded = 3.0; // set to this for now so that i can test the camera input
+	// float rounded = 3.0; // set to this so that i can test the camera input
 	randomNums.push_back(5.0); //apparently i have to push something into the vector first for it to even work
 
 	randomNr = ofRandom(4);
@@ -34,24 +34,46 @@ void Randomizer::draw() {
 
 	if (rounded == 1) {
 
-		runCaptcha.draw();
-		randomNums.push_back(rounded);
 
-		cout << "got 1";
+		if (std::count(randomNums.begin(), randomNums.end(), 1.0)) { // checks if its in the vector 
+
+
+			cout << "We already had this one!";
+
+
+		}
+		else {
+
+			cout << "got 1";
+
+			runCaptcha.draw();
+			randomNums.push_back(rounded);
+
+		}
+
 	}
 
 	else if (rounded == 2) {
 
-		runAudioInput.draw();
-		randomNums.push_back(rounded);
-		cout << "got 2";
+		if (std::count(randomNums.begin(), randomNums.end(), 2.0)) { // checks if its in the vector 
+
+
+			cout << "We already had this one!";
+
+
+		}
+		else {
+
+			cout << "got 2";
+			runAudioInput.draw();
+			randomNums.push_back(rounded);
+
+		}
+
 
 	}
 
 	else if (rounded == 3) {
-
-		cout << "Ok, so this part worked";
-
 
 		//for (int i = 0; i < randomNums.size(); i++) { 
 
@@ -77,10 +99,32 @@ void Randomizer::draw() {
 	}
 	else if (rounded == 4) {
 
-		runTuringQuestion.draw();
-		randomNums.push_back(rounded);
+		if (std::count(randomNums.begin(), randomNums.end(), 4.0)) { // checks if its in the vector 
 
-		cout << "got 4";
+
+			cout << "We already had this one!";
+
+
+		}
+		else {
+
+			cout << "Video input should play now";
+
+			cout << "got 4";
+			runTuringQuestion.draw();
+			randomNums.push_back(rounded);
+
+
+
+
+		}
+
+
+	}
+
+	else {
+
+		cout << "ok, done no more";
 
 	}
 
